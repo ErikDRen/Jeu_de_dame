64c59319e60f80e02b8c424ebeb705cc0bf7ebef
ErikDRen <72856412+ErikDRen@users.noreply.github.com>
2021-11-09 14:07:22 +0100

    selection des pieces

--- Jeu_de_dame-main/Jeu_de_dame-main/src/game/Game.java
+++ Jeu_de_dame-main/Jeu_de_dame-main/src/game/Game.java
@@ -3,6 +3,7 @@ package game;
 import java.util.ArrayList;
 
 import model.Piece;
+import utile.Utilitaires;
 
 public class Game {
 	
@@ -11,20 +12,27 @@ public class Game {
 	
 	char[][] tabMap;
 	
+	boolean gameOn = true;
+	
 	ArrayList<Piece> alPieces = new ArrayList<Piece>();
 	
 	public void game() {
 		// TODO Auto-generated method stub
 		tabMap = new char[sizeX][sizeY];
 		createPiece();
-		fillTab(tabMap, alPieces);
-		printTab(tabMap);
+		do {
+			fillTab(tabMap, alPieces);
+			printTab(tabMap);
+			selectPieceToMove(tabMap,alPieces);			
+		} while (gameOn);
 	}
 
 	private void createPiece() {
 		// TODO Auto-generated method stub
 		Piece p1 = new Piece(1, 1, 'R', false);
+		Piece p2 = new Piece(3, 1, 'R', false);
 		alPieces.add(p1);
+		alPieces.add(p2);
 	}
 
 	private void fillTab(char[][] map, ArrayList<Piece> alPieces) {
@@ -50,6 +58,18 @@ public class Game {
 				System.out.print(map[j][i]);
 			}
 			System.out.println();
+		}
+	}
+	
+	public static void selectPieceToMove(char[][] map, ArrayList<Piece> alPieces) {
+		int selectedX = Utilitaires.giveInt();
+		int selectedY = Utilitaires.giveInt();
+		for (Piece piece : alPieces) {
+			//map[piece.getX()][piece.getY()]= piece.getCouleur();
+			if(selectedX == piece.getX() && selectedY == piece.getY()) {
+				piece.setCouleur('T');
+				//juste un test pour voir si la selection marche
+			}
 		}
 	}
 	
--- Jeu_de_dame-main/Jeu_de_dame-main/src/utile/Utilitaires.java
+++ Jeu_de_dame-main/Jeu_de_dame-main/src/utile/Utilitaires.java
@@ -1,5 +1,17 @@
 package utile;
 
-public class Utilitaires {
+import java.util.Scanner;
 
+public class Utilitaires {
+	public static String giveString() {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		return s;
+	}
+	
+	public static int giveInt() {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		return num;
+	}
 }
