56c5a1574805cdebf7d438f710fd5ad3143ae787
Maxime Derosier <75376645+FeutreEnBois@users.noreply.github.com>
2021-11-09 15:28:40 +0100

    menu part 1

--- Jeu_de_dame-main/Jeu_de_dame-main/src/game/Main.java
+++ Jeu_de_dame-main/Jeu_de_dame-main/src/game/Main.java
@@ -2,7 +2,7 @@ package game;
 
 public class Main {
 	public  static void main(String[] args) {
-		Game g = new Game();
-		g.game();
+		Menu m = new Menu();
+		m.displayMenu();
 	}
 }
---
+++ Jeu_de_dame-main/Jeu_de_dame-main/src/game/Menu.java
@@ -0,0 +1,61 @@
+package game;
+
+import utile.Utilitaires;
+
+public class Menu {
+	
+	public void displayMenu() {
+		boolean gameOn = true;
+		System.out.println("!!! WELCOME TO DRAUGHT GAME !!!");
+		do {
+			chooseNumberOfPlayerMenu();
+			System.out.println("Do you want to continue playing (y-n)?");
+			if(!chooseToContinueMenu()) {
+				gameOn = false;
+			}
+		}while(gameOn);
+	}
+	
+	public void chooseNumberOfPlayerMenu() {
+		boolean appliOn = true;
+		int choice = 0;
+		do {
+			System.out.println("Choose a number of player (btw 1-2) : ");
+			choice = Utilitaires.giveInt();
+
+			switch (choice) {
+			case 1:
+				System.out.println("you choose 1");
+				appliOn = false;
+				break;
+			case 2:
+				System.out.println("you choose 2");
+				startPlayervsPlayerGame();
+				appliOn = false;
+				break;
+			default:
+				System.out.println("Something is wrong, what did you put ??"
+						+ "\nWe only accept Int btw 1-2");
+				break;
+			}
+		} while (appliOn);
+	}
+	
+	private void startPlayervsPlayerGame() {
+		Game g = new Game();
+		g.game();
+	}
+
+
+	private boolean chooseToContinueMenu() {
+		String choice = Utilitaires.giveString();
+		switch(choice) {
+		case "y":
+			return true;
+		case "yes":
+			return true;
+		default:
+			return false;
+		}
+	}
+}
