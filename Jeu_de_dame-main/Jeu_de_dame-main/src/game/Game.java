448f7e01de32215c994fe0ca54e0299b787a423a
ErikDRen <72856412+ErikDRen@users.noreply.github.com>
2021-11-09 15:59:14 +0100

    repush

--- Jeu_de_dame-main/Jeu_de_dame-main/src/game/Game.java
+++ Jeu_de_dame-main/Jeu_de_dame-main/src/game/Game.java
@@ -6,19 +6,12 @@ import model.Piece;
 import utile.Utilitaires;
 
 public class Game {
-<<<<<<< HEAD
-
-	int sizeX = 12;
-	int sizeY = 12;
 
 	int nbPieces = 20;
-
-=======
 	
 	int sizeX = 12;
 	int sizeY = 12;
 	
->>>>>>> 56c5a1574805cdebf7d438f710fd5ad3143ae787
 	char[][] tabMap;
 
 	boolean gameOn = true;
@@ -29,16 +22,12 @@ public class Game {
 		// TODO Auto-generated method stub
 		tabMap = new char[sizeX][sizeY];
 		createPiece();
-		System.out.println(alPieces.toString());
+		//System.out.println(alPieces.toString());
 		do {
 			fillTab(tabMap, alPieces);
-<<<<<<< HEAD
-			printTab(tabMap);
-			selectPieceToMove(tabMap, alPieces);
-=======
+
 			printTab(tabMap,sizeY,sizeX);
 			selectPieceToMove(tabMap,alPieces);			
->>>>>>> 56c5a1574805cdebf7d438f710fd5ad3143ae787
 		} while (gameOn);
 	}
 
@@ -67,15 +56,7 @@ public class Game {
 
 	private void fillTab(char[][] map, ArrayList<Piece> alPieces) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
 
-		for (int i = 0; i < map.length; i++) {
-
-			for (int j = 0; j < map[i].length; j++) {
-				map[i][j] = '.';
-				map[0][j] = '*';
-				map[map.length - 1][j] = '*';
-=======
 		
 		for(int i = 0; i < map.length; i++) {
 			
@@ -84,7 +65,6 @@ public class Game {
 				map[0][j]='*';
 				map[map.length-1][j]='*';
 				
->>>>>>> 56c5a1574805cdebf7d438f710fd5ad3143ae787
 			}
 			map[i][0] = '*';
 			map[i][map[i].length - 1] = '*';
@@ -93,15 +73,7 @@ public class Game {
 			map[piece.getX()][piece.getY()] = piece.getCouleur();
 		}
 	}
-<<<<<<< HEAD
 
-	public static void printTab(char[][] map) {
-		for (int i = 0; i < map.length; i++) {
-			for (int j = 0; j < map[i].length; j++) {
-=======
-	
-	
-	
 	public static void printTab(char[][] map, int sizeY, int sizeX) {
 		char c = 'a';
 		System.out.print("  ");
@@ -120,7 +92,6 @@ public class Game {
 			}
 			
 			for(int j = 0; j<map[i].length; j++) {
->>>>>>> 56c5a1574805cdebf7d438f710fd5ad3143ae787
 				System.out.print(map[j][i]);
 				System.out.print(" ");
 			}
